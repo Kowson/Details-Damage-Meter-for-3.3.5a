8bcc46150858e485c6f29518f9fd0a674a204729
Andrew <andrewseward93@gmail.com>
2020-05-10 07:26:02 -0700

    initial obsidian sanctum

---
+++ Details_RaidInfo-ChamberOfAspects/enUS.lua
@@ -0,0 +1,23 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Details_RaidInfo-ChamberOfAspects-OS", "enUS", true) 
+
+if (not L) then
+	return 
+end 
+
+L["PLUGIN_NAME"] = "Raid Info Obsidian Sanctum"
+L["STRING_RAID_NAME"] = "Obsidian Sanctum"
+
+L["BOSS_SARTHARION"] = "Sartharion"
+L["BOSS_SHADRON"] = "Shadron"
+L["BOSS_TENEBRON"] = "Tenebron"
+L["BOSS_VESPERON"] = "Vesperon"
+
+---------------------------------------------------------------------------------------------------
+local L = LibStub("AceLocale-3.0"):NewLocale("Details_RaidInfo-ChamberOfAspects-RS", "enUS", true) 
+
+if (not L) then
+	return 
+end 
+
+L["PLUGIN_NAME"] = "Raid Info Ruby Sanctum"
+L["STRING_RAID_NAME"] = "Ruby Sanctum"
