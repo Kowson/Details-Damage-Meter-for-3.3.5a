843a0abcddc6af32d418b0055b0a4dcf139c73a7
Andrew <andrewseward93@gmail.com>
2020-05-15 03:50:11 -0700

    disabled drakes in os. messes with 1/2/3d badly

--- DBM-ChamberOfAspects/Obsidian/Shadron.lua
+++ DBM-ChamberOfAspects/Obsidian/Shadron.lua
@@ -14,14 +14,6 @@ mod:RegisterEvents(
 local warnShadowFissure	= mod:NewSpellAnnounce(59127)
 local timerShadowFissure = mod:NewCastTimer(5, 59128)--Cast timer until Void Blast. it's what happens when shadow fissure explodes.
 
-function mod:OnCombatStart()
-    DBM:FireCustomEvent("DBM_EncounterStart", 738, "Shadron")
-end
-
-function mod:OnCombatEnd(wipe)
-	DBM:FireCustomEvent("DBM_EncounterEnd", 738, "Shadron", wipe)
-end
-
 function mod:SPELL_CAST_SUCCESS(args)
     if args:IsSpellID(57579, 59127) and self:IsInCombat() then
         warnShadowFissure:Show()
--- DBM-ChamberOfAspects/Obsidian/Tenebron.lua
+++ DBM-ChamberOfAspects/Obsidian/Tenebron.lua
@@ -14,14 +14,6 @@ mod:RegisterEvents(
 local warnShadowFissure	= mod:NewSpellAnnounce(59127)
 local timerShadowFissure = mod:NewCastTimer(5, 59128)--Cast timer until Void Blast. it's what happens when shadow fissure explodes.
 
-function mod:OnCombatStart()
-    DBM:FireCustomEvent("DBM_EncounterStart", 736, "Tenebron")
-end
-
-function mod:OnCombatEnd(wipe)
-	DBM:FireCustomEvent("DBM_EncounterEnd", 736, "Tenebron", wipe)
-end
-
 function mod:SPELL_CAST_SUCCESS(args)
     if args:IsSpellID(57579, 59127) and self:IsInCombat() then
         warnShadowFissure:Show()
