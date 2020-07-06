239af8fc1197fe65674ccce212ecc318a7facf4f
Kowson <kowsonnn@gmail.com>
2020-07-06 15:24:40 +0200

    Update version number to 1.0.3

--- Details/boot.lua
+++ Details/boot.lua
@@ -4,8 +4,8 @@
 		_ = nil
 		_details = LibStub("AceAddon-3.0"):NewAddon("_details", "AceTimer-3.0", "AceComm-3.0", "AceSerializer-3.0", "NickTag-1.0")
 		_details.build_counter = 100 --it's 101 for release
-		_details.userversion = "v1.0.1"
-		_details.realversion = 2
+		_details.userversion = "v1.0.3"
+		_details.realversion = 4
 		_details.version = _details.userversion .. " (core " .. _details.realversion .. ")"
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
